29fbc62283d33baa6dc44e151362ff31d63df1a2
Franco Miguez <franco.miguez98@gmail.com>
2022-12-05 20:27:55 -0300

    creat funtion for boton and add two image

--- interface.py
+++ interface.py
@@ -174,8 +174,354 @@ class Interface(ctk.CTk):
                                           width=self.SIZE_BUTON,
                                           height=self.SIZE_BUTON,
                                           fg_color="green3",
-                                          hover_color="green"
+                                          hover_color="green",
+                                          command=self.sale
                                           )
+        self.sale_buton.pack(padx=20 , side="left")
+        self.sales_text_total.pack(side="left", padx=self.PADX_BUTON)
+
+    def insert_product(self, product_dicts):
+        """insert 15 product in list stock
+
+        Args:
+            product_dicts (dict): dictionari the products
+        """
+        for product in product_dicts[:15]:
+            data = (
+                    product["product_id"],
+                    product["name"],
+                    product["stock"],
+                    product["price"])
+            self.stock_list.insert('', ctk.END, values=data)
+    
+    def product_delete(self):
+        """delete product the database and stock_list
+        """
+        select_id = self.stock_list.focus()
+        if select_id != "":
+            select = self.stock_list.item(select_id)['values']
+            delete = messagebox.askquestion("Delete",
+                                f"wants delete product {select[1]}?"
+                                )
+            if delete == "yes":
+                db.product_delete(select[0])
+                self.stock_list.delete(select_id)
+    
+    def product_search(self):
+        """search product and update stock_list
+        """
+        text= self.search_entry.get()
+        for product in self.stock_list.get_children():
+            self.stock_list.delete(product)
+            
+        for product in db.product_search(text):
+            data = (
+                    product.product_id,
+                    product.name,
+                    product.stock,
+                    product.price)
+            self.stock_list.insert('', ctk.END, values=data)
+    
+        
+    def window_add(self):
+        """create window for insert data and add product
+        """
+        new_window = ctk.CTkToplevel(self)
+        new_window.geometry("500x600")
+        new_window.resizable(0,0)
+        new_window.title("Add")
+        
+        name_text = ctk.CTkLabel(master=new_window,text="Name:")
+        name_text.cget("font").configure(size=20)
+        name_text.pack(pady=10)
+        name_entry = ctk.CTkEntry(master=new_window,width=300)
+        name_entry.pack()
+        
+        info_text = ctk.CTkLabel(master=new_window,text="Info:")
+        info_text.cget("font").configure(size=20)
+        info_text.pack(pady=10)
+        info_entry = ctk.CTkTextbox(master=new_window,height=150,width=300)
+        info_entry.pack()
+        
+        price_var = ctk.StringVar()
+        price_text = ctk.CTkLabel(master=new_window,text="Price:")
+        price_text.cget("font").configure(size=20)
+        price_text.pack(pady=10)
+        price_entry = ctk.CTkEntry(master=new_window, width=300, textvariable=price_var)
+        price_var.set("0.0")
+        price_entry.pack()
+        
+        stock_var = ctk.StringVar()
+        stock_text = ctk.CTkLabel(master=new_window,text="Stock:")
+        stock_text.cget("font").configure(size=20)
+        stock_text.pack(pady=10)
+        stock_entry = ctk.CTkEntry(master=new_window,width=300, textvariable=stock_var)
+        stock_var.set("0")
+        stock_entry.pack()
+
+        create_boton = ctk.CTkButton(master=new_window,
+                                     image=self.add_image,
+                                     width=50,
+                                     height=50,
+                                     text="",
+                                     fg_color="green3",
+                                     hover_color="green",
+                                     command=lambda:self.product_add(
+                                         new_window,
+                                         name_entry.get(),
+                                         info_entry.get("1.0",ctk.END),
+                                         price_entry.get(),
+                                         stock_entry.get(),
+                                        )
+                                    )
+        create_boton.pack(side="left", padx=100)
+        
+        cancel_boton = ctk.CTkButton(master=new_window,
+                                     image=self.cancel_image,
+                                     width=50,
+                                     height=50,
+                                     text="",
+                                     fg_color="red",
+                                     hover_color="red4",
+                                     command=new_window.destroy
+                                     )
+        cancel_boton.pack(side="right", padx=100)
+
+    def window_update(self):
+        """create window for insert data and update product
+        """
+        select_id = self.stock_list.focus()
+        if select_id != "":
+            select = self.stock_list.item(select_id)['values']
+            select = db.product_info(select[0])
+            for x in select:
+                product = [x["product_id"],
+                            x["name"],
+                            x["info"],
+                            x["price"],
+                            x["stock"]
+                           ]
+
+            
+            new_window = ctk.CTkToplevel(self)
+            new_window.geometry("500x600")
+            new_window.resizable(0,0)
+            new_window.title("Update")
+
+
+            name_text = ctk.CTkLabel(master=new_window,text="Name:")
+            name_text.cget("font").configure(size=20)
+            name_text.pack(pady=10)
+            name_entry = ctk.CTkEntry(master=new_window,width=300)
+            name_entry.insert(0,product[1])
+            name_entry.pack()
+            
+            info_text = ctk.CTkLabel(master=new_window,text="Info:")
+            info_text.cget("font").configure(size=20)
+            info_text.pack(pady=10)
+            info_entry = ctk.CTkTextbox(master=new_window,height=150,width=300)
+            info_entry.insert(ctk.INSERT,product[2])
+            info_entry.pack()
+            
+            price_text = ctk.CTkLabel(master=new_window,text="Price:")
+            price_text.cget("font").configure(size=20)
+            price_text.pack(pady=10)
+            price_entry = ctk.CTkEntry(master=new_window, width=300)
+            price_entry.insert(0,product[3])
+            price_entry.pack()
+            
+            stock_text = ctk.CTkLabel(master=new_window,text="Stock:")
+            stock_text.cget("font").configure(size=20)
+            stock_text.pack(pady=10)
+            stock_entry = ctk.CTkEntry(master=new_window,width=300)
+            stock_entry.insert(0,product[4])
+            stock_entry.pack()
+
+            create_boton = ctk.CTkButton(master=new_window,
+                                        image=self.add_image,
+                                        width=50,
+                                        height=50,
+                                        text="",
+                                        fg_color="green3",
+                                        hover_color="green",
+                                        command=lambda:self.product_update(
+                                            new_window,
+                                            product[0],
+                                            name_entry.get(),
+                                            info_entry.get("1.0",ctk.END),
+                                            price_entry.get(),
+                                            stock_entry.get(),
+                                            )
+                                        )
+            create_boton.pack(side="left", padx=100)
+            
+            cancel_boton = ctk.CTkButton(master=new_window,
+                                        image=self.cancel_image,
+                                        width=50,
+                                        height=50,
+                                        text="",
+                                        fg_color="red",
+                                        hover_color="red4",
+                                        command=new_window.destroy
+                                        )
+            cancel_boton.pack(side="right", padx=100)
+
+    def product_add(self,window,name,info,price,stock):
+        """add product to database
+
+        Args:
+            window (Toplevel): for close 
+            name (str): productname
+            info (str): product info
+            price (float): product price
+            stock (int): product stock
+        """
+        save = True
+        if name == "":
+            messagebox.showwarning( title="Error",message="Insert Name", parent=window)
+            save = False
+        try:
+            if price == "":
+                price = 0
+            else:
+                price = float(price)
+        except:
+            messagebox.showwarning(title="Error", message="insert number value in price", parent=window)
+            save = False
+        
+        try:
+            if stock == "":
+                stock = 0
+            else:
+                stock = int(stock)
+        except:
+            messagebox.showwarning(title="Error", message="insert number value in stock", parent=window)
+            save = False
+        if save == True:
+            db.product_add(name,info,price,stock)
+            window.destroy()
+
+    def product_update(self,window,id,name,info,price,stock):
+        """update product for database
+
+        Args:
+            window (Toplevel): for close
+            id (str): id the product for search in database
+            name (str): for update name
+            info (str): for update info
+            price (float): for update price
+            stock (int): for update stock
+        """
+        save = True
+        if name == "":
+            messagebox.showwarning( title="Error",message="Insert Name", parent=window)
+            save = False
+        try:
+            if price == "":
+                price = 0
+            else:
+                price = float(price)
+        except:
+            messagebox.showwarning(title="Error", message="insert number value in price", parent=window)
+            save = False
+        
+        try:
+            if stock == "":
+                stock = 0
+            else:
+                stock = int(stock)
+        except:
+            messagebox.showwarning(title="Error", message="insert number value in stock", parent=window)
+            save = False
+        if save == True:
+            db.product_update(id,name,info,price,stock)
+            window.destroy()
+
+    def product_add_sale(self):
+        """add a prodct of the stock list to sales list
+        if the sum exists
+        """
+        select_id = self.stock_list.focus()
+        if select_id != "":
+            select = self.stock_list.item(select_id)['values']
+            repit = False
+            for id in self.sales_list.get_children():
+                item = self.sales_list.item(id)['values']
+                if item[0] == select[0]:
+                    repit = True
+                    item[2] = str(int(item[2]) + 1)
+                    item[4] = str(float(item[2]) * float(item[3]))
+                    self.sales_list.item(id, values=item)
+            if not repit:
+                product = [
+                            select[0],
+                            select[1],
+                            1,
+                            select[3],
+                            select[3]
+                        ]
+                self.sales_list.insert('',ctk.END, values=product)
+            self.update_total()
+
+    def product_minus_sale(self):
+        """minus product in sale list 
+        if stock is one the quit
+        """
+        select_id = self.sales_list.focus()
+        if select_id != "":
+            item = self.sales_list.item(select_id)['values']
+            if int(item[2]) == 1:
+                self.sales_list.delete(select_id)
+            else:
+                item[2] = int(item[2]) - 1
+                item[4] = float(item[2]) * float(item[3])
+                self.sales_list.item(select_id, values=item)
+            self.update_total()
+                
+    def product_delete_sale(self):
+        """delte product selection in the sales list
+        """
+        select_id = self.sales_list.focus()
+        if select_id != "":
+            select = self.sales_list.item(select_id)['values']
+            delete = messagebox.askquestion("Delete",
+                                f"wants delete product {select[1]} in sale?"
+                                )
+            if delete == "yes":
+                self.sales_list.delete(select_id)
+                self.update_total()
+
+    def update_total(self):
+        self.total = 0
+        for id in self.sales_list.get_children():
+            self.total += float(self.sales_list.item(id)["values"][4])
+        total = "{:,}".format(self.total)
+        self.sales_text_total.configure(text=f"Total: $ {total}")
+
+    def sale(self):
+        id_sale = self.sales_list.get_children()
+        if len(id_sale) > 0:
+            new_window = ctk.CTkToplevel(self)
+            new_window.geometry("300x200")
+            new_window.resizable(0,0)
+            new_window.title("Pay")
+            
+            pay_text = ctk.CTkLabel(new_window,text="Pay with:")
+            pay_text.pack(pady=10)
+            pay_entry = ctk.CTkEntry(new_window)
+            pay_entry.pack(pady=10)
+            
+            pay_boton = ctk.CTkButton(master=new_window,
+                                     image=self.dollar_image,
+                                     width=50,
+                                     height=50,
+                                     text="",
+                                     fg_color="green3",
+                                     hover_color="green",
+                                     command=lambda:self.pay(id_sale,
+                                                             pay_entry.get(),
+                                                             new_window)
+                                     )
         self.sale_buton.pack(pady=self.PADX_BUTON)
 
 
